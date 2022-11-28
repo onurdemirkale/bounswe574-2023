f5fdb2dd5d7b604bad2eba21070c499860aa7dd0
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-28 19:43:15 +0300

    Updated annotation view to pass user authentication information in the context

--- swe574/apps/annotation/views.py
+++ swe574/apps/annotation/views.py
@@ -4,8 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def annotations_view(request):
-    context: {
+    
+    user_authenticated = False
+    user_id = None
+    
+    if request.user.is_authenticated:
+        user_authenticated = True
+        user_id = request.user.id
 
+    context = {
+        'user_authenticated': user_authenticated,
+        'user_id': user_id
     }
 
-    return render(request, 'annotation/annotations.html')
+    return render(request, 'annotation/annotations.html', context)
