af736464b31bc9124fb73b58ce8f6c75f09c7538
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-28 15:30:39 +0300

    Initialized the annotation Django app

---
+++ swe574/apps/annotation/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def annotations_view(request):
+    context: {
+
+    }
+
+    return render(request, 'annotation/annotations.html')
