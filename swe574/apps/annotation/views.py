83f2973528a1f6f7fa5d83af536f5af645fb962a
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-25 03:49:20 +0300

    Updated annotations view name and implemented a template view to create annotations

--- swe574/apps/annotation/views.py
+++ swe574/apps/annotation/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+def my_annotations_view(request):
 
-def annotations_view(request):
-    
     user_authenticated = False
     user_id = None
-    
+
     if request.user.is_authenticated:
         user_authenticated = True
         user_id = request.user.id
@@ -16,5 +14,24 @@ def annotations_view(request):
         'user_authenticated': user_authenticated,
         'user_id': user_id
     }
+
+    return render(request, 'annotation/annotations.html', context)
+
+
+def create_annotation_view(request):
+
+    user_authenticated = False
+    user_id = None
+
+    if request.user.is_authenticated:
+        user_authenticated = True
+        user_id = request.user.id
+
+    context = {
+        'user_authenticated': user_authenticated,
+        'user_id': user_id
+    }
+
+    print('creating an annotation...')
 
     return render(request, 'annotation/annotations.html', context)
