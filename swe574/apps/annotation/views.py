5e11c1c39b31d1fdc304423860d9511501ed9f1f
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-27 20:11:05 +0300

    Update to obtain the host and target URI from the request

--- swe574/apps/annotation/views.py
+++ swe574/apps/annotation/views.py
@@ -30,7 +30,13 @@ def create_annotation_view(request):
 
     # Obtain annotation server URI from environment variables
     annotation_server_uri = os.environ['ANNOTATION_SERVER_URI']
-    }
+
+    # Obtain the current host to display in annotation
+    host = request.get_host()
+
+    # Obtain the URI of the page that the annotation was generated in
+    target_uri = request.META.get('HTTP_REFERER')
+
 
     # TODO: Replace the request method with a POST request and include
     # annotation content in the data.
