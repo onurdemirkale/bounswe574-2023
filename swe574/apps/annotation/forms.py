ab6725738beff32af37df36f00f3952bdc37f620
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-27 19:31:01 +0300

    Update forms for text position start and end fields

--- swe574/apps/annotation/forms.py
+++ swe574/apps/annotation/forms.py
@@ -6,3 +6,5 @@ class AnnotationForm(forms.Form):
     target_type = forms.CharField()
     target_value = forms.CharField()
     target_xpath = forms.CharField()
+    text_position_start = forms.CharField(required=False)
+    text_position_end = forms.CharField(required=False)
